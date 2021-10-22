d562e11f08669c4076d4cd44367444f91d2e7c58
lajanslime <85597365+lajanslime@users.noreply.github.com>
2021-10-21 21:30:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace("postgres://", "postgresql://", 1) or "sqlite:///db.sqlite"
-
+AWS=os.environ.get("AWS")
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -30,7 +30,7 @@ Pet = create_classes(db)
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", AWS=AWS)
 
 
 # Query the database and send the jsonified results
